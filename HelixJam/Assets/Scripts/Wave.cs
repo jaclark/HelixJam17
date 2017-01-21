83ece548d8b540376bffe2a547d7d7259384c717
jagraybox <jaclark.games@gmail.com>
2017-01-20 20:52:23 -0800

    First pass on the main mechanic.

--- HelixJam/Assets/Scripts/Wave.cs
+++ HelixJam/Assets/Scripts/Wave.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class Wave : MonoBehaviour 
 {
-	public float upSpeed = 0.0f;
-	public float frequency = 0.0f;
-	public float amplitude = 0.0f;
-	public bool reverse = false;
+	public float speed = 0.0f;
+	[Range(0.1f,1.0f)]
+	public float gravity = 0.0f;
+
+	[Range(0.1f, 1.0f)]
+	public float maxInfluence = 0.0f;
+	public float influenceAcceleration = 0.0f;
+	public float influenceDeceleration = 0.0f;
+
+	private float _influence = 0.0f;
+
+	private float _xMove = 0.0f;
 
 	private void Update()
 	{
 		Vector3 delta = Vector3.zero;
 
-		delta.y = upSpeed;
+		delta.y = speed;
 
-		if (reverse)
+		if (Input.GetKey (KeyCode.Space))
 		{
-			delta.x = Mathf.Sin (Time.time * frequency) * amplitude;
+			_influence += influenceAcceleration;
 		}
 		else
 		{
-			delta.x = - Mathf.Sin(Time.time * frequency) * amplitude;
+			_influence -= influenceDeceleration;
 		}
+		_influence = Mathf.Clamp (_influence, 0, maxInfluence);
+
+		_xMove += -transform.position.x * (gravity + _influence);
+		delta.x = _xMove;
 
 		transform.position += delta * Time.deltaTime;
 	}
