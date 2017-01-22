af3e0fa50fb6a32f06ea8285c1afc5a53c3ab4ee
Matt <mattscorca@gmail.com>
2017-01-21 18:20:39 -0800

    audiomanager

---
+++ HelixJam/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource Melody;
+	public AudioSource Beat;
+	public Transform Wave;  
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Wave.position.x != 0) Melody.volume = 1 - Mathf.Abs(Wave.position.x) / 15;
+		if (Wave.position.x != 0) Beat.volume = 1 - Mathf.Abs (Wave.position.x) / 15;
+
+	}
+}
