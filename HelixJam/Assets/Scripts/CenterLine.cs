6fde4135395d712d05b765e64c89a8b250e96da1
jagraybox <jaclark.games@gmail.com>
2017-01-21 16:40:36 -0800

    Added shader for Center Line.

---
+++ HelixJam/Assets/Scripts/CenterLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterLine : MonoBehaviour 
+{
+	private Renderer _renderer = null;
+
+	private void Start()
+	{
+		_renderer = GetComponent<Renderer> ();
+	}
+
+	public void SetColors(Color a, Color b)
+	{
+		_renderer.material.SetColor ("_ColorA", a);
+		_renderer.material.SetColor ("_ColorB", b);
+	}
+
+	public void SetPositions(float a, float b)
+	{
+		_renderer.material.SetFloat ("_XPosA", a);
+		_renderer.material.SetFloat ("_XPosB", b);
+	}
+}
